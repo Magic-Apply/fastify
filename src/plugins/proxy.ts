96f6badf8ebd6f83cfae18673479776c8bce5437
Matei Canavra <tools@matei.work>
2024-12-11 01:12:16 -0500

    CORS was working all along, but redocly + requestly + retool all use proxy servers, so CORS doesn't apply

--- src/plugins/proxy.ts
+++ src/plugins/proxy.ts
@@ -3,8 +3,11 @@ import fp from "fastify-plugin";
 import httpProxy from "@fastify/http-proxy";
 import { FastifyRequest, FastifyInstance } from "fastify";
 import fastifyIp from "fastify-ip";
+
+
 const isProduction =
 	!process.env.RUN_LOCAL || process.env.RUN_LOCAL === "false";
+
 const upstreamUrl = isProduction
 	? String(process.env.INTERNAL_API_BASE_URL)
 	: String(process.env.INTERNAL_API_BASE_URL_LOCAL);
@@ -12,6 +15,8 @@ const upstreamUrl = isProduction
 const internalAPIHost = isProduction
 	? String(process.env.INTERNAL_API_HOST)
 	: String(process.env.INTERNAL_API_HOST_LOCAL);
+
+const logLevel = isProduction ? "info" : "trace";
 
 export default fp(async (fastify) => {
 	await fastify.register(fastifyIp, {
@@ -24,8 +29,7 @@ export default fp(async (fastify) => {
 		upstream: upstreamUrl,
 		prefix: `/${process.env.PUBLIC_API_OPERATIONS_PATH}`,
 		rewritePrefix: `/${process.env.INTERNAL_API_OPERATIONS_PATH}/`, // Fix trailing slash, make condidional on empty /
-		logLevel: "trace",
-		disableCache: true,
+		logLevel: logLevel,
 		preHandler: async (request, reply) => {
 			fastify.log.info("PREHANDLER OPERATIONS REQUEST");
 			if (request.url === "/favicon.ico") {
@@ -77,7 +81,7 @@ export default fp(async (fastify) => {
 		upstream: upstreamUrl,
 		prefix: `/${process.env.PUBLIC_API_WEBHOOKS_PATH}`,
 		rewritePrefix: `/${process.env.INTERNAL_API_WEBHOOKS_PATH}/`, // Fix trailing slash, make condidional on empty /
-		logLevel: "trace",
+		logLevel: logLevel,
 		preHandler: async (request, reply) => {
 			// Access the Host and Origin headers from the original request
 			fastify.log.info("PREHANDLER WEBHOOKS REQUEST");
@@ -111,55 +115,34 @@ export default fp(async (fastify) => {
 
 function printRequest(request: FastifyRequest, fastify: FastifyInstance) {
 	// Access the Host and Origin headers from the original request
-	fastify.log.info(`Request URL: ${request.url}`);
-	fastify.log.info(`Request IP: ${request.ip}`);
-	fastify.log.info(`Request IPs: ${request.ips}`);
-	fastify.log.info(`Request Headers: ${request.headers["authority"]}`);
-	fastify.log.info(`Request Host: ${request.headers["host"]}`);
-	fastify.log.info(`Request Origin: ${request.headers["origin"]}`);
-	fastify.log.info(`Request Referer: ${request.headers["referer"]}`);
-	fastify.log.info(
-		`Request X-Forwarded-For: ${request.headers["x-forwarded-for"]}`
-	);
-	fastify.log.info(
-		`Request X-Forwarded-Host: ${request.headers["x-forwarded-host"]}`
-	);
-	fastify.log.info(
-		`Request X-Forwarded-Origin: ${request.headers["x-forwarded-origin"]}`
-	);
-	fastify.log.info(
-		`Request X-Forwarded-Proto: ${request.headers["x-real-ip"]}`
-	);
-	fastify.log.info(`Request From: ${request.headers["from"]}`);
-	fastify.log.info(`Request User-Agent: ${request.headers["user-agent"]}`);
-	fastify.log.info(`Request Forwarded: ${request.headers["forwarded"]}`);
-	fastify.log.info(
-		`Request X-Forwarded-Proto: ${request.headers["x-forwarded-proto"]}`
-	);
-	fastify.log.info(
-		`Request X-Forwarded-Port: ${request.headers["x-forwarded-port"]}`
-	);
-	fastify.log.info(
-		`Request X-Forwarded-Scheme: ${request.headers["x-forwarded-scheme"]}`
-	);
-	fastify.log.info(
-		`Request X-Forwarded-User: ${request.headers["x-forwarded-user"]}`
-	);
-	fastify.log.info(
-		`Request X-Forwarded-Method: ${request.headers["x-forwarded-method"]}`
-	);
-	fastify.log.info(
-		`Request X-Forwarded-Uri: ${request.headers["x-forwarded-uri"]}`
-	);
-	fastify.log.info(
-		`Request X-Forwarded-Ssl: ${request.headers["x-forwarded-ssl"]}`
-	);
-	fastify.log.info(
-		`Request X-Forwarded-Server: ${request.headers["x-forwarded-server"]}`
-	);
-	fastify.log.info(
-		`Request X-Forwarded-Scheme: ${request.headers["x-forwarded-scheme"]}`
-	);
-	fastify.log.info(`Request Connection: ${request.headers["connection"]}`);
-	fastify.log.info(`Request Range: ${request.headers["range"]}`);
+	const logData = {
+		requestUrl: request.url,
+		requestIp: request.ip,
+		requestIps: request.ips,
+		requestHeaders: {
+			authority: request.headers["authority"],
+			host: request.headers["host"],
+			origin: request.headers["origin"],
+			referer: request.headers["referer"],
+			xForwardedFor: request.headers["x-forwarded-for"],
+			xForwardedHost: request.headers["x-forwarded-host"],
+			xForwardedOrigin: request.headers["x-forwarded-origin"],
+			xRealIp: request.headers["x-real-ip"],
+			from: request.headers["from"],
+			userAgent: request.headers["user-agent"],
+			forwarded: request.headers["forwarded"],
+			xForwardedProto: request.headers["x-forwarded-proto"],
+			xForwardedPort: request.headers["x-forwarded-port"],
+			xForwardedScheme: request.headers["x-forwarded-scheme"],
+			xForwardedUser: request.headers["x-forwarded-user"],
+			xForwardedMethod: request.headers["x-forwarded-method"],
+			xForwardedUri: request.headers["x-forwarded-uri"],
+			xForwardedSsl: request.headers["x-forwarded-ssl"],
+			xForwardedServer: request.headers["x-forwarded-server"],
+			connection: request.headers["connection"],
+			range: request.headers["range"]
+		}
+	};
+
+	fastify.log.info(logData);
 }
